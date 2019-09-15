ca42c540d136a8b0b7637c84f04a7a0016de9c32
shigenobu <shigenobu314@yahoo.co.jp>
2019-09-15 12:56:17 +0900

    例外クラス追加

---
+++ MIDIClockCSWrapper/MIDIClockLibException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIDIClockCSWrapper
+{
+	class MIDIClockLibException : Exception
+	{
+		public MIDIClockLibException() : base("MIDIClockライブラリで不明なエラーが発生しました。")
+		{
+		}
+		public MIDIClockLibException(string message) : base(message)
+		{
+		}
+		public MIDIClockLibException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+	}
+}
